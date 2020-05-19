b6883235445c9c09c954921b0375c87d16bd7136
CD065208 <chandrakanth.dudela@cerner.com>
2020-05-19 08:59:43 -0400

    Stack and Queue Impl

--- src/main/java/Queue.java
+++ src/main/java/Queue.java
@@ -1,3 +1,42 @@
 public class Queue {
 
+  Node head;
+  Node tail;
+
+  public Queue(Node head) {
+    this.head = head;
+    this.tail = head;
+  }
+
+  public boolean isEmpty() {
+    return head == null;
+  }
+
+  public int peek() {
+    if (head == null) {
+      return -1; //throw some exception
+    }
+    return head.data;
+  }
+
+  public void add(int data) {
+    Node newNode = new Node(data);
+    if (head == null) {
+      head = newNode;
+      return;
+    }
+    tail.next = newNode;
+    tail = newNode;
+  }
+
+  public int remove() {
+    if (head == null) {
+      return -1; //throw some exception
+    }
+    int data = head.data;
+    if (head.next != null) {
+      head = head.next;
+    }
+    return data;
+  }
 }
---
+++ src/main/java/Stack.java
@@ -0,0 +1,36 @@
+public class Stack {
+  Node top;
+
+  public Stack(Node top) {
+    this.top = top;
+  }
+
+  public boolean isEmpty() {
+    return top == null;
+  }
+
+  public int peek() {
+    if (top == null) {
+      return -1; //throw some exception
+    }
+    return top.data;
+  }
+
+  public void add(int data) {
+    Node newNode = new Node(data);
+    if (top == null) {
+      top = newNode;
+    }
+    newNode.next = top;
+    top = newNode;
+  }
+
+  public int remove() {
+    if (top == null) {
+      return -1; //throw some exception
+    }
+    int data = top.data;
+    top = top.next;
+    return data;
+  }
+}
